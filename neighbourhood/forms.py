78ee7a0d81c1de8a96a146250ab127a48a12c23c
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-16 17:40:41 +0300

    add a form for business

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Neighbourhood
+from .models import Neighbourhood,Business
 
 class NeighbourhoodForm(forms.ModelForm):
     class Meta:
         model = Neighbourhood
-        fields = [' neighbourhood_name',' neighbourhood_location','occupation_count']
+        fields = [' neighbourhood_name',' neighbourhood_location','occupation_count'] 
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = ['business_name',' business_email_address']
