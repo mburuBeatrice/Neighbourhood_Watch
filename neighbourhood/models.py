3884fe0edc449556f41bccdfe963a1054c0bd231
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-16 17:43:20 +0300

    add a models for business

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 # Create your models here.
@@ -7,7 +6,7 @@ class Neighbourhood(models.Model):
     neighbourhood_name = models.CharField(max_length=30)
     neighbourhood_location = models.CharField(max_length=30)
     occupation_count = models.CharField(max_length=30)
-    user = models.ForeignKey(User,default=1)
+    user = models.ForeignKey('User')
 
     def __str__(self):
         return self.neighbourhood_name
@@ -16,6 +15,8 @@ class User(models.Model):
     name = models.CharField(max_length=30)
     neighbourhood = models.ForeignKey(Neighbourhood)
     email_address = models.EmailField()
+
+
 
 class Business(models.Model):
     business_name = models.CharField(max_length=30)
