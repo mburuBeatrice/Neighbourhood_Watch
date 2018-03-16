c83618a088adfe8c8efbc598d830d0226935ca05
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-16 15:48:52 +0300

    adds a function to display all the neighbourhoods

---
+++ neighbourhood/views.py
@@ -0,0 +1,46 @@
+from .models import Neighbourhood
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib import messages
+from .forms import NeighbourhoodForm
+# Create your views here.
+def index(request):
+    queryset = Neighbourhood.objects.all()
+
+    context = {
+        "object_list":queryset,
+        "neighbourhood_name": "name"
+    }
+    return render(request, 'index.html',context)
+
+# def find_neighbourhood(request, id):
+
+#     instance = get_object_or_404(Neighbourhood, id=id)
+#     context = {
+#         "neighbourhood_name":instance.neighbourhood_name,
+#         "instance":instance 
+#     }
+#     return render(request, 'index.html', context)
+# def create_neighbourhood(request):
+#     form = NeighbourhoodForm(request.POST or None, request.FILES or None)
+#     # current_user = request.user
+#     if form.is_valid():
+#         instance = form.save(commit=False)
+#         instance.save()
+#         messages.success(request, "Successfully created")
+#         return HttpResponseRedirect(instance.get_absolute_url())
+#     else:
+#         messages.error(request, "Not successfully created")
+
+
+#     context = {
+#         "form" : form,
+#     }
+
+#     return render(request, 'neighbourhood_form.html', context)
+
+# delete_neigborhood()
+
+
+# update_neighborhood()
+# update_occupants()
