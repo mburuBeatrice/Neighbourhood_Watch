9fe5dbba71e69afc3750e306e68862c1250eb5a4
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-18 22:28:13 +0300

    add django registration

--- neighbourhood_watch/urls.py
+++ neighbourhood_watch/urls.py
@@ -19,6 +19,8 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('neighbourhood.urls'))
+    url(r'',include('neighbourhood.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
 ]
 
