85ccba87cf2cf71a109441592a5b0361ece8b59e
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-18 19:10:58 +0300

    adds forms for contacts and posts

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Neighbourhood,Business
+from .models import Neighbourhood,Business,Posts
 
 class NeighbourhoodForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,8 @@ class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         fields = ['business_name',' business_email_address']
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Posts
+        fields = ['post']
